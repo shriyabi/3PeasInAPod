920d9db84b82e42a85f714a71c901b0e06c8a1ae
Nathanael Lu <lunathanael@gmail.com>
2024-09-28 14:25:57 -0400

    Modified server connection manager logic

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from core import Connection
 
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
-@app.websocket("/ws")
-async def websocket(websocket: WebSocket):
-    connection: Connection = Connection(websocket)
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[Connection] = []
+
+    async def connect(self, websocket: WebSocket):
+        connection: Connection = await Connection.connect(websocket)
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager: ConnectionManager = ConnectionManager()
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
