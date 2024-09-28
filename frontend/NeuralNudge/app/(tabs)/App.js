fef49128970ee6ab364a998b4f602b815a3fac1e
shriyabi <shriyarbiddala@gmail.coom>
2024-09-28 16:53:53 -0400

    ahh

---
+++ frontend/NeuralNudge/app/(tabs)/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import HomeScreen from './index'; // Import your HomeScreen component
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <HomeScreen />
+    </View>
+  );
+}
